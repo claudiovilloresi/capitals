03c4f0c4e7d26db602d52028e8075239925041c7
albertopossagno <alberto.possagno@gmail.com>
2019-11-20 14:26:53 +0100

    Modifying Main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python3
 
-from capitals import check_capital, check_state
+from capitals import capitals.py,
 
 check_capital("Germany")
 check_capital("Honduras")
 check_state("Rome")
 check_state("Tokyo")
+
 
