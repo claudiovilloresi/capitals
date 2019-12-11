3a8a8080ff91a9e692b46a6bb1aa33a2d09ebeb5
matteotomiazzo <matthewtomiazzo@gmail.com>
2019-12-11 22:35:10 +0100

    Fixed test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,23 +3,29 @@ from main import parse_allowed_input
 import os
 
 
+
 class TestMain(unittest.TestCase):
 
+#open the test
     def setUp(self):
-        # note: this would be better done with tempfile
-        self.temporary_file = "/tmp/emptyfile"
+        self.temporary_file = "/tmp/test_empty_datafile"
         f = open(self.temporary_file, 'w')
         f.close()
 
-    def test_no_datafile(self):
+#CSV tests
+#test 1
+    def test_no_datafile(self): #test the csv
         u, r = parse_allowed_input(datafile="/tmp/nonexistentfile-wewefwwe")
         self.assertFalse(u)
         self.assertFalse(r)
 
-    def test_empty_datafie(self):
+#test 2
+
+    def test_empty_datafie(self): #test the datafile in an empty situation
         u, r = parse_allowed_input(datafile=self.temporary_file)
         self.assertFalse(u)
         self.assertFalse(r)
 
+#close the test
     def tearDown(self):
         os.remove(self.temporary_file)
