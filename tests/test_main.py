0130eca14e8088bbae4b0f35c6035abef1ee3228
Riccardo Bonacina <riccardo.bonacina@student.h-farm.com>
2019-12-13 16:40:30 +0100

    PEP8 implementation

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,28 +14,29 @@ import unittest
 from main import parse_allowed_input
 import os
 
+
 class TestMain(unittest.TestCase):
 
-#open the test
+    # open the test
     def setUp(self):
         self.temporary_file = "/tmp/test_empty_datafile"
         f = open(self.temporary_file, 'w')
         f.close()
 
-#CSV tests
-#test 1
-    def test_no_datafile(self): #test the csv
+# CSV tests
+# test 1
+    def test_no_datafile(self):  # test the csv
         u, r = parse_allowed_input(datafile="/tmp/nonexistentfile-wewefwwe")
         self.assertFalse(u)
         self.assertFalse(r)
 
-#test 2
+# test 2
 
-    def test_empty_datafie(self): #test the datafile in an empty situation
+    def test_empty_datafie(self):  # test the datafile in an empty situation
         u, r = parse_allowed_input(datafile=self.temporary_file)
         self.assertFalse(u)
         self.assertFalse(r)
 
-#close the test
+# close the test
     def tearDown(self):
         os.remove(self.temporary_file)
